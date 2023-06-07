bf7738f3d53c88eab994e2f6c4527fa68a7dfe1d
bilasyivaa <nabila04@mhs.mdp.ac.id>
2023-06-07 17:38:18 +0700

    Go-Relax 3

---
+++ app/src/main/java/com/example/go_relax/AddUnggahActivity.java
@@ -0,0 +1,16 @@
+package com.example.go_relax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddUnggahActivity extends AppCompatActivity {
+
+    private AddUnggahActivity binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_unggah);
+    }
+}
