15f5d4fe22abf977d958cdd0a80efa28afd86344
bilasyivaa <nabila04@mhs.mdp.ac.id>
2023-06-07 14:21:12 +0700

    Go Relax 2

---
+++ app/src/main/java/com/example/go_relax/utilities.java
@@ -0,0 +1,37 @@
+package com.example.go_relax;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class utilities {
+
+    public static final String PREFERENCE_FILE_KEY = utilities.class.getPackage().getName();
+
+    public static void clearuser(Context context) {
+        SharedPreferences sp = context.getSharedPreferences(PREFERENCE_FILE_KEY, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("xUsername", null);
+        editor.apply();
+    }
+
+    public static void setValue(Context context, String xPref, String xValue){
+        SharedPreferences sp = context.getSharedPreferences(PREFERENCE_FILE_KEY, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString(xPref, xValue);
+        editor.apply();
+
+    }
+
+    public static String getValue(Context context, String xPref){
+        SharedPreferences sp = context.getSharedPreferences(PREFERENCE_FILE_KEY, Context.MODE_PRIVATE);
+        return sp.getString(xPref, null);
+
+    }
+
+    public static boolean checkValue(Context context, String xPref){
+        SharedPreferences sp = context.getSharedPreferences(PREFERENCE_FILE_KEY, Context.MODE_PRIVATE);
+        String xValue = sp.getString(xPref, null);
+        return xValue != null;
+    }
+}
+
