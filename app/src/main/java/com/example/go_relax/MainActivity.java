7b35330e530469e8b1727a97a6de8590906dd90a
bilasyivaa <nabila04@mhs.mdp.ac.id>
2023-06-09 10:59:59 +0700

    Go Relax 4

--- app/src/main/java/com/example/go_relax/MainActivity.java
+++ app/src/main/java/com/example/go_relax/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.go_relax;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.go_relax.databinding.ActivityMainBinding;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private UnggahViewAdapter unggahViewAdapter;
+    private List<Unggah> data;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        if (!utilities.checkValue(MainActivity.this, "xUsername")) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
+
+        unggahViewAdapter = new UnggahViewAdapter();
+        binding.rvUnggah.setLayoutManager(new LinearLayoutManager(this));
+        binding.rvUnggah.setAdapter(unggahViewAdapter);
+
+        binding.fabInput.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddUnggahActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
-}
